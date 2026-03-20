c0cb974fe5adf2572f24ec0fa61e8a30bbfaf2b6
kokoron <78990112+ILKokoron@users.noreply.github.com>
2026-03-20 11:52:23 +0700

    Enhance next.config.js with Webpack and experimental settings

--- next.config.js
+++ next.config.js
@@ -1,3 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    // Copy WASM files to output
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: 'asset/resource',
+    });
+
+    if (isServer) {
+      config.externals = [...(config.externals || []), '@shelby-protocol/sdk'];
+    }
+
+    return config;
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@shelby-protocol/sdk', '@shelby-protocol/clay-codes'],
+  },
+};
+
 module.exports = nextConfig;
